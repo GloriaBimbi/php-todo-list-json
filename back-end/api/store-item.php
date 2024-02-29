c10a6e0aa3e82c7c0f90274c9d990c8ad7d3e95a
GloriaBimbi <gloria.bimbi1@libero.it>
2024-02-29 16:59:55 +0100

    feat new item

---
+++ back-end/api/store-item.php
@@ -0,0 +1,25 @@
+<?php
+
+//salvo l'oggetto composto dal tezt inserito dall'utente e dalla chiave done falsa come da default, ottenute da una richiesta axios in post, in una variabile
+$newElement = $_POST['newElement'];
+
+//leggo il contenuto del file json
+$json_todolist = file_get_contents('../data/todolist.json');
+
+//trasformo il json in un array del php in modo tale che il php riesca a comprenderlo
+$todolist_array = json_decode($json_todolist, true);
+
+//salvo solo il contenuto dell'array degli elementi della to do in una variabile (così da poter successivamente inserire il nuovo elemento direttamente dentro di esso invece  che nell'array che contiene questo array)
+$todolist = $todolist_array['toDoElements'];
+
+//aggiungo all'array del file json l'oggetto del nuovo elemento inserito dall'utente
+$todolist[] = $newElement;
+
+//salvo nel json la il nuovo array
+$json_new_list = json_encode($todolist);
+
+//avviso il browser che sta per riceve un file json
+header('Content-Type: application/json');
+
+//stampo la nuova lista di cose da fare dentro il file json (dopo aver cancellato tutti gli eventuali var_dump per evitare errori)
+echo $json_new_list;
--- front-end/js/main.js
+++ front-end/js/main.js
@@ -22,15 +22,31 @@ const app = createApp({
     //   delateToDoElement(i) {
     //     this.toDoElements.splice(i, 1);
     //   },
-    //   addNewToDoElement() {
-    //     const newToDoElements = { ...this.newToDoElement };
-    //     if (this.newToDoElement.text != "") {
-    //       this.toDoElements.push(newToDoElements);
-    //       this.newToDoElement.text = "";
-    //     } else {
-    //       alert("Attenzione! Non hai dato un nome alla nuova task");
-    //     }
-    //   },
+    addNewToDoElement() {
+      const newElement = { ...this.newToDoElement };
+      console.log("Item da aggiungere: " + newElement.text);
+      if (this.newToDoElement.text != "") {
+        //definisco i parametri che poi passerò nella richiesta axios e li inserisco all'inteno di una varibaile
+        const data = {
+          newElement,
+        };
+        //creo una variabile che contiene le indicazioni per l'interstazione della richiesta axios
+        const params = {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        };
+        // faccio una richiesta axios sfruttando le variabili vcreate in precedenza per scriverla in maniera più chiara
+        axios
+          .post("../back-end/api/store-item.php", data, params)
+          .then((response) => {
+            this.toDoElements = response.data;
+          });
+      } else {
+        alert("Attenzione! Non hai dato un nome alla nuova task");
+      }
+      this.newToDoElement.text = "";
+    },
   },
   mounted() {
     this.fetchtoDoElements();
